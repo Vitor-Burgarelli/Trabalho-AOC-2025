39280f33da088206221093a97e48bde64152c57b
Vitor Burgarelli <90009178+Vitor-Burgarelli@users.noreply.github.com>
2025-11-27 18:46:05 -0300

    Add files via upload

--- abcd.asm
+++ abcd.asm
@@ -1,3 +1,17 @@
+funcao:
+	load 0
+	xto Y
+	load 1
+	xto R4 	
+
+pisca:
+	BC
+	xto R4
+	BS
+	xto R4
+	goto pisca
+
+
 start:
 	load 0
 	xto M
@@ -17,13 +31,14 @@ ALOCA:
 	xto M
 	tox R0
 	movexa 15
-	goto ponteiro
 
 	load 13
 	xto Y
 	tox R0
 	EQ
 	IF
+	
+	goto ponteiro
 	goto fim
 RECURSIVIDADE:
 	tox R1
@@ -35,14 +50,6 @@ RECURSIVIDADE:
 	ADD
 	xto R2
 	goto ALOCA
-
-	;R2 -> R1
-	;R1 -> R0
-	;R0 -> X
-	;R1 ->	Y
-	;ADD
-	;X->R2
-	;goto ponteiro
 
 
 ponteiro:
